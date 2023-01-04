7c9b51bcf5aeb712fe3d23888bb576036d47fb49
Pranav <rajan.pranav320@gmail.com>
2023-01-03 19:39:58 -0800

    Svelte-D3 scatterplot

---
+++ simple-scatterplot/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  // Logic goes here (JavaScript, Svelte)
+  import data from '$data/data.js';
+  import { scaleLinear } from 'd3-scale';
+  import { max } from 'd3-array';
+  import AxisX from '$components/AxisX.svelte';
+  let width = 400;
+  let height = 400;
+  let xScale = scaleLinear().domain([0, 100]).range([0, width]);
+  let yScale = scaleLinear()
+    .domain([0, max(data, (d) => d.hours)])
+    .range([height, 0]);
+  // console.log(data);
+</script>
+
+<main>
+  <!-- Markup goes here (HTML, SVG) -->
+  <svg {width} {height}>
+    <AxisX {height} {xScale} />
+    {#each data as d}
+      <circle
+        cx={xScale(d.grade)}
+        cy={yScale(d.hours)}
+        r={10}
+        fill="cornflowerblue"
+        stroke="black"
+        stroke-width={1}
+      />
+    {/each}
+  </svg>
+</main>
+
+<style>
+  /* Styles go here (CSS) */
+</style>
---
+++ simple-scatterplot/src/components/AxisX.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let height;
+  //   export let width;
+  export let xScale;
+  //   export let margin;
+
+  let xTicks = [0, 25, 50, 75, 100];
+</script>
+
+<g class="axis x" transform="translate(0, {height})">
+  {#each xTicks as tick}
+    <g class="tick" transform="translate({xScale(tick)}, 0)">
+      <!-- <line /> -->
+      <text>{tick}%</text>
+    </g>
+  {/each}
+</g>
+
+<style></style>
